0b6a6d5cfe0e44c6aa119c51a072f4552a3f1a3e
dendavidov-dev <125741421+dendavidov-dev@users.noreply.github.com>
2023-02-25 12:36:01 +0000

    ci: improve conventional changelog (#327)
    Co-authored-by: dendavidov <mail@dendavidov.com>

--- commitlint.config.js
+++ commitlint.config.js
@@ -2,7 +2,11 @@ module.exports = {
   extends: ['@commitlint/config-conventional'],
   // more rules: https://commitlint.js.org/#/reference-rules
   rules: {
-    'type-enum': [2, 'always', ['feat', 'fix', 'test', 'build', 'chore', 'perf', 'docs']],
+    'type-enum': [
+      2,
+      'always',
+      ['feat', 'fix', 'test', 'build', 'chore', 'perf', 'refactor', 'docs', 'style', 'ci'],
+    ],
     'subject-empty': [2, 'never'],
   },
 };
